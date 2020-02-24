58605d78906ac3762b9f196276fe282b5ffbd979
Ojo Oluwasetemi <setemiojo@gmail.com>
2020-02-24 13:13:49 +0100

    📦 NEW: Add command line argument parsing

--- index.ts
+++ index.ts
@@ -1,13 +1,16 @@
 import { serve } from "https://deno.land/std@v0.33.0/http/server.ts";
 import { red } from "https://deno.land/std@v0.33.0/fmt/colors.ts";
+import { parse } from "https://deno.land/std/flags/mod.ts";
 
-if (Deno.args.length === 0 || Deno.args.length > 1) {
+if (Deno.args.length === 0 || Deno.args.length > 2) {
   console.error(red("Need a handler"));
   Deno.exit(1);
 }
 
-const s = serve({ port: 8000 });
-console.log("http://localhost:8000/");
+let port = parse(Deno.args)['port'] || 8000;
+
+const s = serve({ port });
+console.log(`http://localhost:${port}/`);
 
 const handlerPath = await Deno.realpath(Deno.args[0]);
 const handler = await import(handlerPath);
