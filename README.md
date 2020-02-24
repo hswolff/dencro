2db91b437cbc8eb9e33c40fd2788ebb8b38ee4a6
Harry Wolff <hswolff@gmail.com>
2020-02-23 21:52:35 -0500

    tweaaaks

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ an adaptation of [micro](https://github.com/zeit/micro) for [deno](https://deno.
 ```
  deno --allow-net --allow-read index.ts handler.ts
 ```
+
+## Todo
+
+- [ ] Add [command line argument parsing](https://deno.land/std/flags/)
+  - So we can set the port
+- [ ] ok
--- index.ts
+++ index.ts
@@ -1,15 +1,16 @@
 import { serve } from "https://deno.land/std@v0.33.0/http/server.ts";
-const s = serve({ port: 8000 });
+import { red } from "https://deno.land/std@v0.33.0/fmt/colors.ts";
 
-console.log("http://localhost:8000/");
-
-if (Deno.args.length !== 1) {
-  throw new Error("Need a handler!");
+if (Deno.args.length === 0 || Deno.args.length > 1) {
+  console.error(red("Need a handler"));
+  Deno.exit(1);
 }
+
+const s = serve({ port: 8000 });
+console.log("http://localhost:8000/");
 
 const handlerPath = await Deno.realpath(Deno.args[0]);
 const handler = await import(handlerPath);
-console.log({ handler });
 
 for await (const req of s) {
   console.log(req.url);
