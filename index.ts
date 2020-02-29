8a6565c3f842de83a2384a3773a47de1a14f93de
Harry Wolff <hswolff@gmail.com>
2020-02-29 13:14:23 -0500

    feat: Add support to set the port that we serve content from

--- index.ts
+++ index.ts
@@ -1,15 +1,22 @@
 import { serve } from "https://deno.land/std@v0.35.0/http/server.ts";
 import { red } from "https://deno.land/std@v0.35.0/fmt/colors.ts";
+import { parse } from "https://deno.land/std@v0.35.0/flags/mod.ts";
 
-if (Deno.args.length === 0 || Deno.args.length > 1) {
+const args = parse(Deno.args, {
+  default: {
+    port: 8080
+  }
+});
+
+if (args._.length === 0 || args._.length > 1) {
   console.error(red("Need a handler"));
   Deno.exit(1);
 }
 
-const s = serve({ port: 8000 });
-console.log("http://localhost:8000/");
+const s = serve({ port: args.port });
+console.log(`http://localhost:${args.port}/`);
 
-const handlerPath = await Deno.realpath(Deno.args[0]);
+const handlerPath = await Deno.realpath(args._[0]);
 const handler = await import(handlerPath);
 
 for await (const req of s) {
