e5ec74935736ab680cf4b2a2d37a80d29dfcdd7f
Harry Wolff <hswolff@gmail.com>
2020-03-10 20:54:57 -0400

    Move most of the top level code into createServer

--- index.ts
+++ index.ts
@@ -5,68 +5,73 @@ import {
 import { parse } from "flags";
 import { red, green } from "fmt/colors";
 
-const args = parse(Deno.args, {
-  default: {
-    port: 8080
-  }
-});
+async function createServer() {
+  const args = parse(Deno.args, {
+    default: {
+      port: 8080
+    }
+  });
 
-// Allow a user to set what directory to serve files from
-if (args._.length === 1) {
-  const directoryArg = args._[0];
-  try {
-    const fullPath = await Deno.realpath(directoryArg);
-    Deno.chdir(fullPath);
-  } catch (error) {
-    console.error(red(`Unable to serve files from: ${directoryArg}`));
+  // Allow a user to set what directory to serve files from
+  if (args._.length === 1) {
+    const directoryArg = args._[0];
+    try {
+      const fullPath = await Deno.realpath(directoryArg);
+      Deno.chdir(fullPath);
+    } catch (error) {
+      console.error(red(`Unable to serve files from: ${directoryArg}`));
+    }
+  }
+
+  console.log("Serving files from:", Deno.cwd());
+
+  const s = serve({ port: args.port });
+  console.log(`Listening at http://localhost:${args.port}/`);
+
+  for await (const req of s) {
+    if (req.url.endsWith("favicon.ico")) {
+      req.respond({
+        status: 200
+      });
+      continue;
+    }
+
+    const logRequest = createLogRequest(req);
+
+    let handlerPath;
+    try {
+      handlerPath = await getHandlerPath(req.url);
+    } catch (error) {
+      req.respond({
+        status: 404,
+        body: `Error: No handler for ${req.url}`
+      });
+      logRequest(false);
+      continue;
+    }
+
+    try {
+      const handler = await import(handlerPath);
+
+      req.respond({
+        body: handler.default(req)
+      });
+
+      logRequest(true);
+    } catch {
+      req.respond({
+        status: 500,
+        body: `Error: Unable to parse handler at ${req.url}`
+      });
+      logRequest(false);
+    }
   }
 }
-
-console.log("Serving files from:", Deno.cwd());
-
-const s = serve({ port: args.port });
-console.log(`Listening at http://localhost:${args.port}/`);
-
-for await (const req of s) {
-  if (req.url.endsWith("favicon.ico")) {
-    req.respond({
-      status: 200
-    });
-    continue;
-  }
-
-  const logRequest = createLogRequest(req);
-
-  let handlerPath;
-  try {
-    handlerPath = await getHandlerPath(req.url);
-  } catch (error) {
-    req.respond({
-      status: 404,
-      body: `Error: No handler for ${req.url}`
-    });
-    logRequest(false);
-    continue;
-  }
-
-  try {
-    const handler = await import(handlerPath);
-
-    req.respond({
-      body: handler.default(req)
-    });
-
-    logRequest(true);
-  } catch {
-    req.respond({
-      status: 500,
-      body: `Error: Unable to parse handler at ${req.url}`
-    });
-    logRequest(false);
-  }
+if (import.meta.main) {
+  createServer();
 }
 
-async function getHandlerPath(requestUrl: string): Promise<string> {
+export async function getHandlerPath(requestUrl: string): Promise<string> {
   const requestAsFile = requestUrl.endsWith("/")
     ? `${requestUrl}index`
     : requestUrl;
