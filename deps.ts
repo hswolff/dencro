ba4f518440fcad8f5f80e2fbc73d2ab8d832b727
Maximous Black <maximousblk@gmail.com>
2020-05-12 20:20:59 +0530

    formatting

--- deps.ts
+++ deps.ts
@@ -1,6 +1,3 @@
-export {
-  serve,
-  ServerRequest
-} from "https://deno.land/std/http/server.ts";
+export { serve, ServerRequest } from "https://deno.land/std/http/server.ts";
 export { parse } from "https://deno.land/std/flags/mod.ts";
 export { red, green } from "https://deno.land/std/fmt/colors.ts";
