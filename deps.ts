dd7b05a9d9a6e972abe05fa8a476b91d59084542
Maximous Black <maximousblk@gmail.com>
2020-05-12 20:19:28 +0530

    Create deps.ts

---
+++ deps.ts
@@ -0,0 +1,6 @@
+export {
+  serve,
+  ServerRequest
+} from "https://deno.land/std/http/server.ts";
+export { parse } from "https://deno.land/std/flags/mod.ts";
+export { red, green } from "https://deno.land/std/fmt/colors.ts";
