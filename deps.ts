0abcca0c91dd9cdadfcd0e84fc1165c94f489a9d
Maximous Black <maximousblk@gmail.com>
2020-05-21 14:56:56 +0530

    specified version numbers in deps

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export { serve, ServerRequest } from "https://deno.land/std/http/server.ts";
-export { parse } from "https://deno.land/std/flags/mod.ts";
-export { red, green } from "https://deno.land/std/fmt/colors.ts";
+export { serve, ServerRequest } from "https://deno.land/std@0.52.0/http/server.ts";
+export { parse } from "https://deno.land/std@0.52.0/flags/mod.ts";
+export { red, green } from "https://deno.land/std@0.52.0/fmt/colors.ts";
